27fe9e07077d9f8ec38c301315072f7c705cb8d1
Trevor Kimenye <kimenye@gmail.com>
2013-01-28 21:24:11 +0300

    Added sponsors

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Nys::Application.routes.draw do
 
   root :to => "home#index", :as => :home_page
 
+  get "sponsors" => "home#sponsors", :as => "sponsors"
+
   # This line mounts Refinery's routes at the root of your application.
   # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
