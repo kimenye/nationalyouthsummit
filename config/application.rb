fa75522353a68007dae6193a1b2e5e32a714b88d
Trevor Kimenye <kimenye@gmail.com>
2013-01-30 13:05:49 +0300

    Changed partners and sponsors

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module Nys
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0.1'
+    config.assets.version = '1.0.2'
 
     Refinery::Core.configure do |config|
       config.s3_backend = true
